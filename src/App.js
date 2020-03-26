c1ab18e4d3a63b3a091bc2816fb2094fb4107f4f
Caio Poli <caio.poli@ernetwork.com.br>
2020-03-26 12:13:02 -0300

    :sparkles: Add new Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import Header from './Header';
+
 function App() {
   return (
-    <h1> Be The Hero </h1>
+    <Header>
+      Week OmniStack
+    </Header>
   );
 }
 
