11d8dac280ce2f691d2d9a563abc3aab9b560430
Caio Poli <caio.poli@ernetwork.com.br>
2020-03-26 12:56:17 -0300

    :lipstick: Creating login screen style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 
-import Header from './Header';
+import './global.css'
+
+import Login from './pages/Login';
 
 function App() {
 
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header>Counter: {counter}</Header>
-      <button onClick={increment}>Increment</button>
-    </div>
+    <Login />
   );
 }
 
